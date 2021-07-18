5ba86c0019d20aa73e35a535ca950b9bf284a68f
jesuspena91 <jesusp@seas.upenn.edu>
2021-07-17 19:07:31 -0500

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -10,4 +10,66 @@ session = DBSession()
 
 def process_order(order):
     #Your code here
+    
+    # Check if there are any existing orders that match
+    query = (session.query(Order)
+              .filter(Order.filled == None)
+              .filter(Order.buy_currency == order['sell_currency'])
+              .filter(Order.sell_currency == order['buy_currency'])
+              .filter((Order.sell_amount/Order.buy_amount) >= (order['buy_amount']/order['sell_amount']))
+              )
+    
+    # Inserting order in database
+    new_order = Order( sender_pk=order['sender_pk'],
+                      receiver_pk=order['receiver_pk'], 
+                      buy_currency=order['buy_currency'], 
+                      sell_currency=order['sell_currency'], 
+                      buy_amount=order['buy_amount'], 
+                      sell_amount=order['sell_amount'] )
+    session.add(new_order)
+    session.commit()
+    
+    if query.count() > 0:
+      existing_order = query.first()
+      
+      # Set the filled field to be the current timestamp on both orders
+      new_order.filled = datetime.now()
+      existing_order.filled = datetime.now()
+      session.commit()
+      
+      # Set counterparty_id to be the id of the other order
+      new_order.counterparty_id = existing_order.id
+      existing_order.counterparty_id = new_order.id
+      session.commit()
+      
+      # If one of the orders is not completely filled 
+      # (i.e. the counterparty’s sell_amount is less than buy_amount)
+      if new_order.buy_amount < existing_order.sell_amount:
+        remaining_buy = existing_order.sell_amount - new_order.buy_amount
+        remaining_sell = existing_order.buy_amount - new_order.sell_amount
+        
+        derived_order = Order( sender_pk=existing_order.sender_pk,
+                      receiver_pk=existing_order.receiver_pk, 
+                      buy_currency=existing_order.buy_currency, 
+                      sell_currency=existing_order.sell_currency, 
+                      buy_amount=remaining_buy, 
+                      sell_amount=remaining_sell,
+                      creator_id=existing_order.id)
+        session.add(derived_order)
+        session.commit()
+      
+      elif new_order.buy_amount > existing_order.sell_amount:
+        remaining_buy = new_order.buy_amount - existing_order.sell_amount
+        remaining_sell = new_order.sell_amount - existing_order.buy_amount
+        
+        derived_order = Order( sender_pk=new_order.sender_pk,
+                      receiver_pk=new_order.receiver_pk, 
+                      buy_currency=new_order.buy_currency, 
+                      sell_currency=new_order.sell_currency, 
+                      buy_amount=remaining_buy, 
+                      sell_amount=remaining_sell,
+                      creator_id=new_order.id)
+        session.add(derived_order)
+        session.commit()
+    
     pass
